d0636ea9ed6eee7c2794a61368e8abe22979e3df
SSoltani83 <skander.soltani@baruchmail.cuny.edu>
2021-07-01 22:34:16 -0700

    Added two Modules: SentimentModule and NetworkModule

---
+++ climate_scanner/__init__.py
@@ -0,0 +1,13 @@
+class SentimentModule:
+    # Class defining the Sentiment Analytics Module
+    # Methods:
+    #     method1:...
+    def __init__(selt):
+        pass
+
+class NetworkModule:
+    # Class defining the Network Analytics kModule
+    # Methods:
+    #     method1:...
+    def __init__(self):
+        pass
