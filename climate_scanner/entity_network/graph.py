4ed38df26d00ec84060ff04d6b27df917344d365
Eliot Frazier <eliot@cogn>
2021-07-02 10:09:06 +0100

    Add a useful utility function

--- climate_scanner/entity_network/graph.py
+++ climate_scanner/entity_network/graph.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+#############################################################################
+#
+# 	A necessary utility for accessing the data local to the installation.
+#
+#############################################################################
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_data(path):
+	return os.path.join(_ROOT, 'data', path)
+
 
 class Node:
 	# Class defining the Node
