91dfe5b07fe120cc5cace5c5e5ee225905704a3e
Eliot Frazier <eliot@cogn>
2021-07-02 10:06:49 +0100

    Create sub-module for the entity graph

---
+++ climate_scanner/entity_network/graph.py
@@ -0,0 +1,26 @@
+
+class Node:
+	# Class defining the Node
+	# Methods:
+	#     method1:...
+	# Attributes:
+	def __init__(self):
+		pass
+
+
+class EntityGraph:
+	# Class defining the Entity Graph
+	# Methods:
+	#     method1:...
+	# Attributes:
+	def __init__(self):
+		pass
+
+
+class GraphConstructor:
+	# Class for faccilitating the construction of an entity graph
+	# Methods:
+	#     method1:...
+	# Attributes:
+	def __init__(self):
+		pass
