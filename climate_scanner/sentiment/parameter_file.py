d5ca566c9d1813e2a8d40bfef6609760b6dd35fc
SSoltani83 <skander.soltani@baruchmail.cuny.edu>
2021-07-06 09:37:36 -0700

    added parameter file

---
+++ climate_scanner/sentiment/parameter_file.py
@@ -0,0 +1,20 @@
+#############################################################################
+#
+# 	File to store all hyper-parameters for Pre-processing and Sentiment Model
+#
+#############################################################################
+
+
+
+
+# Pre-processing parameters:
+#....
+
+
+# Model Hyper-Parameters:
+#...
+
+
+
+# Optimization parameters:
+#...
