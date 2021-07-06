2d6b0a7835aae41120d8b65b8fbca50cce641ae3
SSoltani83 <skander.soltani@baruchmail.cuny.edu>
2021-07-05 23:19:45 -0700

    Added training data file to data repo \n Created class skeleton that included: 1.added  parameter_file.py ,2.In sentiment.py Made 'classifier class' inherited from' Keras.Model' 3.In sentiment.py Added data_preprocessing class

---
+++ climate_scanner/sentiment/parameter_file.py
@@ -0,0 +1,15 @@
+#############################################################################
+#
+# 	File to store all hyper-parameters for Pre-processing and Sentiment Model
+#
+#############################################################################
+
+
+
+
+# Pre-processing parameters:
+#....
+
+
+# Model Parameters:
+#...
