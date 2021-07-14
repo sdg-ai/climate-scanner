2cf47297d0622d36412ddf9595495def2e5e62f9
Sonaalll <sonal.singh98@gmail.com>
2021-07-14 21:07:51 +0530

    adding input/outputs

--- climate_scanner/coarse_article_classifier/main.py
+++ climate_scanner/coarse_article_classifier/main.py
@@ -1,10 +1,11 @@
 from climate_scanner.coarse_article_classifier.coarse_classifier import classifier
 
+classifier_obj = classifier()
 
 path = ""
-data = classifier.load_data(path)
-transformed_data = classifier.pre_processing(data)
-model = classifier.fit(transformed_data)
-predictions = classifier.predict(model,transformed_data)
+data = classifier_obj.load_data(path)
+transformed_data = classifier_obj.pre_processing(data)
+model = classifier_obj.fit(transformed_data)
+predictions = classifier_obj.predict(model,transformed_data)
 
-classifier.evaluate(predictions)
+classifier_obj.evaluate(predictions)
