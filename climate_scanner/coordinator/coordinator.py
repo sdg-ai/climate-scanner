7e3d4d79f785891954e973871d9c397ae1a6123e
Eliot Frazier <eliot@cogn>
2021-07-20 10:59:18 +0100

    Add coordinator process

--- climate_scanner/coordinator/coordinator.py
+++ climate_scanner/coordinator/coordinator.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import os
-# from climate_scanner.coarse_article_classifier import...
-# from climate_scanner.trends_innovation_classifier import...
-# from climate_scanner.sentiment import...
+import json
+from climate_scanner.coarse_article_classifier.coarse_classifier import Classifier
+from climate_scanner.trends_innovation_classifier.trends_innovation_classifier import Doc2Trend
+from climate_scanner.sentiment.sentiment_classifier import interface
 
 
 #############################################################################
@@ -21,11 +22,36 @@ def get_data(path):
 
 class EnrichmentCoordinator:
 	def __init__(self):
-		self.coarse_classifier = None
-		self.innovations_classifer = None
-		self.trends_classifier = None
-		self.sentiment_classifier = None
+		self.coarse_classifier = Classifier()
+		self.i_t_classifer = Doc2Trend()
+		self.sentiment_classifier = interface()
 		self.entity_recognition = None
 
-	def process(self):
-		pass
+	def process(self, input_json):
+		output_1 = self.coarse_classifier.predict(input_json)
+
+		output_2 = self.i_t_classifer.coordinator_pipeline(output_1, 0.5)
+
+		output_3 = self.sentiment_classifier.text_to_sentiment(output_2)
+
+		return output_3
+
+
+def run_example():
+	ec = EnrichmentCoordinator()
+	example_data = json.load(open(get_data('example_input.jsonl'), 'rt', encoding='utf-8', errors='ignore'))
+
+	print('=============	Doc 	=============')
+	print('ID:', '\t', example_data['id'])
+	print('Title:', '\t', example_data['title'])
+	print('Document:', '\t', example_data['Document'])
+
+	output_data = ec.process(example_data)
+
+	print('=============	Enriched Doc 	=============')
+	print(json.dumps(output_data))
+
+
+if __name__ == '__main__':
+	run_example()
+
