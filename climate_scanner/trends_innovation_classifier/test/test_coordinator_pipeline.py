42b12ab41dbda327e17208c5d1721e114856519b
Vaishnavi <v.kandala@elsevier.com>
2021-07-30 21:10:45 +0530

    commiting corrections for trends innovation classifier

--- climate_scanner/trends_innovation_classifier/test/test_coordinator_pipeline.py
+++ climate_scanner/trends_innovation_classifier/test/test_coordinator_pipeline.py
@@ -4,8 +4,8 @@ from trends_innovation_classifier import *
 class MyTestCase(unittest.TestCase):
     def test_coordinator_pipeline(self):
         input_dictionary = {"ID": 1545,"title": "The Shattering Truth of 3D-Printed Clothing","doc": "Three-dimensional printing has changed the way we make everything from prosthetic limbs to aircraft parts and even homes. Now it may be poised to upend the apparel industry as well.","climate_scanner":True,"climate_scanner_prob": 0.6}
-        cp = Doc2Trends(input_dictionary,0.4)
-        output = cp.coordinator_pipeline()
+        cp = Doc2Trends()
+        output = cp.coordinator_pipeline(input_dictionary,0.4)
         print(output)
 
 if __name__ == '__main__':
