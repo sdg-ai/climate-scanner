d2c6c7ee709ea291d0604a5097ad8adcdcc681de
Skander Soltani <skandersoltani@Skanders-MacBook-Pro.local>
2021-09-16 22:10:50 -0700

    removing code

--- climate_scanner/entity_network/entity_extraction.py
+++ climate_scanner/entity_network/entity_extraction.py
@@ -15,8 +15,10 @@
 import requests
 import os
 
+
+
 # Set via environment variables
-user_key = os.getenv('user_key')
+user_key = ""
 
 headers = {
 
@@ -54,6 +56,35 @@ headers = {
 #######################################################
 
 
+import urllib.parse, urllib.request, json
+
+def CallWikifier(text, lang="en", threshold=0.8):
+    # Prepare the URL.
+    data = urllib.parse.urlencode([
+        ("text", text), ("lang", lang),
+        ("userKey", "lrccenpxcbtbmpvabyjudwvxehjcpf"),
+        ("pageRankSqThreshold", "%g" % threshold), ("applyPageRankSqThreshold", "true"),
+        ("nTopDfValuesToIgnore", "200"), ("nWordsToIgnoreFromList", "200"),
+        ("wikiDataClasses", "true"), ("wikiDataClassIds", "false"),
+        ("support", "true"), ("ranges", "false"), ("minLinkFrequency", "2"),
+        ("includeCosines", "false"), ("maxMentionEntropy", "3")
+        ])
+    url = "http://www.wikifier.org/annotate-article"
+    # Call the Wikifier and read the response.
+    req = urllib.request.Request(url, data=data.encode("utf8"), method="POST")
+    with urllib.request.urlopen(req, timeout = 60) as f:
+        response = f.read()
+        response = json.loads(response.decode("utf8"))
+
+	# Output the annotations.
+    for annotation in response["annotations"]:
+        print("%s (%s)" % (annotation["title"], annotation["url"]))
+	
+	
+
+
+
+
 def get_entities(extraction_string, min_page_rank=-1, min_link_freq=1):
 	if min_page_rank != -1:
 		apply_pagerank_threshold = True
@@ -69,7 +100,7 @@ def get_entities(extraction_string, min_page_rank=-1, min_link_freq=1):
 			   'applyPageRankSqThreshold': apply_pagerank_threshold}
 
 	r = requests.post(api_url, headers=headers, data=payload)
-
+	print(r.json())
 	if r.status_code == 200:
 		return r.json()
 
