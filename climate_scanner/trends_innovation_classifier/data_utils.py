00704acdfbae11991ac22b8a84458a244d4cdde4
DESKTOP-MOLA860\my-pc <cmwase@ieee.org>
2021-12-07 19:54:54 +0800

    Added code to catch missing keys

--- climate_scanner/trends_innovation_classifier/data_utils.py
+++ climate_scanner/trends_innovation_classifier/data_utils.py
@@ -49,10 +49,13 @@ def pull_categoryData(inputData,category:str):
     :return: outputData - jsonl file - List of dictionaries containing the raw training dataset for the specified category
     """
     outputData = []
-    for i,d in enumerate(inputData):
-        if category in d['category']:
-            outputData.append(d.copy())
-    return outputData if outputData else print("Incorrect category name!")
+    if 'category' not in inputData[0].keys():
+        print("The provided dataset has no key named 'category'!")
+    else:
+        for i,d in enumerate(inputData):
+            if category in d['category']:
+                outputData.append(d.copy())
+        return outputData if outputData else print("Incorrect category name!")
 
 
 def doc_to_sentence(text:str):
@@ -84,78 +87,81 @@ def build_trainingData(inputData):
     params = load_params()
     num_sentences = params['pre_training']['num_sentences']
     
-    for d in inputData:
-        count, sent_id = 0, 0
-        id_array, pos_array, text_array = [], [], []
-        inputText = d.get('text') 
-        outputText['id'] = d.get('id')
-        outputText['title'] = d.get('title')
-        splitText = doc_to_sentence(inputText)
-        for st in splitText:
-            sent_id = count
-            sent_pos = st.get('string_indices')
-            sent_text = st.get('text')
-            id_array.append(sent_id)
-            pos_array.append(sent_pos)
-            text_array.append(sent_text)
-            count += 1
-        count = 0
-        quotient = math.ceil(len(text_array)/num_sentences)
-        remainder = math.ceil(len(text_array)%num_sentences)
-        ta_range = quotient
-        for ta in range(ta_range):
-            if remainder == 0 or (remainder > 0 and ta < quotient - 1):
-                k_range = num_sentences
-            elif (remainder > 0 and ta == quotient - 1):
-                k_range = remainder
-            elif (len(text_array) == 1):
-                k_range = 1
-            for k in range(k_range):
-                if len(text_array) == 1:
-                    outputText['prev_id'] = []
-                    outputText['prev_pos'] = []
-                    outputText['prev_text'] = ""
-                    outputText['curr_id'] = id_array[count]
-                    outputText['curr_pos'] = pos_array[count]
-                    outputText['curr_text'] = text_array[count]
-                    outputText['next_id'] = []
-                    outputText['next_pos'] = []
-                    outputText['next_text'] = ""
-                elif count == 0:
-                    outputText['prev_id'] = []
-                    outputText['prev_pos'] = []
-                    outputText['prev_text'] = ""
-                    outputText['curr_id'] = id_array[count]
-                    outputText['curr_pos'] = pos_array[count]
-                    outputText['curr_text'] = text_array[count]
-                    outputText['next_id'] = id_array[count+1]
-                    outputText['next_pos'] = pos_array[count+1]
-                    outputText['next_text'] = text_array[count+1]
-                elif count == len(text_array)-1:
-                    outputText['prev_id'] = id_array[count-1]
-                    outputText['prev_pos'] = pos_array[count-1]
-                    outputText['prev_text'] = text_array[count-1]
-                    outputText['curr_id'] = id_array[count]
-                    outputText['curr_pos'] = pos_array[count]
-                    outputText['curr_text'] = text_array[count]
-                    outputText['next_id'] = []
-                    outputText['next_pos'] = []
-                    outputText['next_text'] = ""
-                else:
-                    outputText['prev_id'] = id_array[count-1]
-                    outputText['prev_pos'] = pos_array[count-1]
-                    outputText['prev_text'] = text_array[count-1]
-                    outputText['curr_id'] = id_array[count]
-                    outputText['curr_pos'] = pos_array[count]
-                    outputText['curr_text'] = text_array[count]
-                    outputText['next_id'] = id_array[count+1]
-                    outputText['next_pos'] = pos_array[count+1]
-                    outputText['next_text'] = text_array[count+1]
-                outputText['category'] = d.get('category')
-                outputText['climate_scanner'] = d.get('climate_scanner')
-                outputData.append(outputText.copy())
+    if not all(k in inputData[0].keys() for k in ('text','id','title','category','climate_scanner')):
+        print("The provided dataset is missing one or more required keys!")
+    else:
+        for d in inputData:
+            count, sent_id = 0, 0
+            id_array, pos_array, text_array = [], [], []
+            inputText = d.get('text') 
+            outputText['id'] = d.get('id')
+            outputText['title'] = d.get('title')
+            splitText = doc_to_sentence(inputText)
+            for st in splitText:
+                sent_id = count
+                sent_pos = st.get('string_indices')
+                sent_text = st.get('text')
+                id_array.append(sent_id)
+                pos_array.append(sent_pos)
+                text_array.append(sent_text)
                 count += 1
-    return outputData
+            count = 0
+            quotient = math.ceil(len(text_array)/num_sentences)
+            remainder = math.ceil(len(text_array)%num_sentences)
+            ta_range = quotient
+            for ta in range(ta_range):
+                if remainder == 0 or (remainder > 0 and ta < quotient - 1):
+                    k_range = num_sentences
+                elif (remainder > 0 and ta == quotient - 1):
+                    k_range = remainder
+                elif (len(text_array) == 1):
+                    k_range = 1
+                for k in range(k_range):
+                    if len(text_array) == 1:
+                        outputText['prev_id'] = []
+                        outputText['prev_pos'] = []
+                        outputText['prev_text'] = ""
+                        outputText['curr_id'] = id_array[count]
+                        outputText['curr_pos'] = pos_array[count]
+                        outputText['curr_text'] = text_array[count]
+                        outputText['next_id'] = []
+                        outputText['next_pos'] = []
+                        outputText['next_text'] = ""
+                    elif count == 0:
+                        outputText['prev_id'] = []
+                        outputText['prev_pos'] = []
+                        outputText['prev_text'] = ""
+                        outputText['curr_id'] = id_array[count]
+                        outputText['curr_pos'] = pos_array[count]
+                        outputText['curr_text'] = text_array[count]
+                        outputText['next_id'] = id_array[count+1]
+                        outputText['next_pos'] = pos_array[count+1]
+                        outputText['next_text'] = text_array[count+1]
+                    elif count == len(text_array)-1:
+                        outputText['prev_id'] = id_array[count-1]
+                        outputText['prev_pos'] = pos_array[count-1]
+                        outputText['prev_text'] = text_array[count-1]
+                        outputText['curr_id'] = id_array[count]
+                        outputText['curr_pos'] = pos_array[count]
+                        outputText['curr_text'] = text_array[count]
+                        outputText['next_id'] = []
+                        outputText['next_pos'] = []
+                        outputText['next_text'] = ""
+                    else:
+                        outputText['prev_id'] = id_array[count-1]
+                        outputText['prev_pos'] = pos_array[count-1]
+                        outputText['prev_text'] = text_array[count-1]
+                        outputText['curr_id'] = id_array[count]
+                        outputText['curr_pos'] = pos_array[count]
+                        outputText['curr_text'] = text_array[count]
+                        outputText['next_id'] = id_array[count+1]
+                        outputText['next_pos'] = pos_array[count+1]
+                        outputText['next_text'] = text_array[count+1]
+                    outputText['category'] = d.get('category')
+                    outputText['climate_scanner'] = d.get('climate_scanner')
+                    outputData.append(outputText.copy())
+                    count += 1
+        return outputData
     
 
 def format_outputData(outputData:list):
@@ -167,23 +173,26 @@ def format_outputData(outputData:list):
     
     outputText,metaData = {},{}
     prodigyData = []
-    for d in outputData:
-        outputText={}
-        outputText['doc_id'] = d.get('id')
-        outputText['sent_id']=d.get('curr_id')
-        if d.get('prev_pos')==[]:
-            outputText['sent_start_pos']=[] 
-        else: 
-            outputText['sent_start_pos']=d.get('prev_pos')[0] 
-        if d.get('next_pos')==[]: 
-            outputText['sent_end_pos']=[] 
-        else:
-            outputText['sent_end_pos']=d.get('next_pos')[1]
-        outputText['title'] = d.get('title')
-        metaData["meta"]=outputText
-        metaData["text"]=d.get('prev_text')+' '+d.get('curr_text')+' '+d.get('next_text')
-        prodigyData.append(metaData.copy())
-    return prodigyData
+    if not all(k in outputData[0].keys() for k in ('id','curr_id','prev_pos','next_pos','title','prev_text','curr_text','next_text')):
+        print("The provided dataset is missing one or more required keys!")
+    else:
+        for d in outputData:
+            outputText={}
+            outputText['doc_id'] = d.get('id')
+            outputText['sent_id']=d.get('curr_id')
+            if d.get('prev_pos')==[]:
+                outputText['sent_start_pos']=[] 
+            else: 
+                outputText['sent_start_pos']=d.get('prev_pos')[0] 
+            if d.get('next_pos')==[]: 
+                outputText['sent_end_pos']=[] 
+            else:
+                outputText['sent_end_pos']=d.get('next_pos')[1]
+            outputText['title'] = d.get('title')
+            metaData["meta"]=outputText
+            metaData["text"]=d.get('prev_text')+' '+d.get('curr_text')+' '+d.get('next_text')
+            prodigyData.append(metaData.copy())
+        return prodigyData
 
             
 def save_outputData(outputData:list):
