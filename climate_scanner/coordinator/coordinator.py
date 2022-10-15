11a8ff9844d8ab7f6d758ddc9366670f1149d7d8
Eliotfrazier <eliot@cognism.com>
2022-10-15 21:25:01 +0100

    Add example additional code

--- climate_scanner/coordinator/coordinator.py
+++ climate_scanner/coordinator/coordinator.py
@@ -4,8 +4,10 @@ import os
 import json
 
 from climate_scanner.entity_network.graph_demo.entity_extraction import EntityExtractor
+from climate_scanner.noise_classifier.body_classifier import BodyNonbodyClassifier
 from climate_scanner.sentiment_classifier.sentiment_classifier import SentimentInterface
 from climate_scanner.trends_innovation_classifier.trends_innovation_classifier import TrendsInnovationClassifier
+import nltk
 
 # Initialize Graph Handler and Entity Extractor Objects
 # graph = GraphConstructor()
@@ -30,6 +32,7 @@ class EnrichmentCoordinator:
         self.d2t = TrendsInnovationClassifier()
         self.d2s = SentimentInterface()
         self.entity_recognition = EntityExtractor()
+        self.cleaner = BodyNonbodyClassifier()
 
     def dummy_process(self, input_json):
         output_1 = self.d2c.get_climate_class(input_json)
@@ -40,30 +43,51 @@ class EnrichmentCoordinator:
         print('3: ', output_3)
         return output_3
 
+    def clean_text(self, text):
+        sentences = nltk.sent_tokenize(text)
+        cleaned_sentences = []
+        for sentence in sentences:
+            prediction = self.cleaner.predict(sentence)
+            if prediction == 'BODY':
+                cleaned_sentences.append(sentence)
+        return ' '.join(cleaned_sentences)
+
+
     def process(self, article_text):
+        article_text = self.clean_text(article_text)
         trends_present_set = []
         trend_results = self.d2t.scan_predict(article_text)
         sentiment = self.d2s.text_to_sentiment(article_text)[0]
-        entities = self.entity_recognition.get_annotations(article_text)
+        # entities = self.entity_recognition.get_annotations(article_text)
+        entities = []
+        entities_dedupe_set = set()
         for item in trend_results:
             item['extract_sentiment'] = self.d2s.text_to_sentiment(item['text'])
             entity_list = []
             for entity in self.entity_recognition.get_annotations(item['text']):
                 if entity[0] not in entity_list:
                     entity_list.append(entity[0])
+                    if entity[0] not in entities_dedupe_set:
+                        entities.append(entity)
+                        entities_dedupe_set.add(entity[0])
 
             item['extract_entities'] = entity_list
             print(item)
 
         results = {'trend_extractions': trend_results,
                    'article_sentiment': sentiment,
-                   'article_entities': entities}
+                   'article_entities': entities,
+                   # TODO: replace with real model
+                   'climate_relevance_score':0.95}
 
+        print(len(trend_results))
         return results
 
 
 def run_example():
+    # Example json
     ec = EnrichmentCoordinator()
+    '''
     example_data = json.load(open(get_data('example_input.jsonl'), 'rt', encoding='utf-8', errors='ignore'))
 
     print('=============	Doc 	=============')
@@ -72,9 +96,21 @@ def run_example():
     print('Document:', '\t', example_data['doc'])
 
     output_data = ec.process(example_data['doc'])
+    json.dump(output_data, open(get_data('example_output.jsonl'), 'wt', encoding='utf-8', errors='ignore'))
+    '''
 
-    print('=============	Enriched Doc 	=============')
-    print(json.dumps(output_data))
+    # Example txt
+
+    f = open(get_data('525.txt'), 'rt', encoding='utf-8', errors='ignore')
+
+    text = f.read()
+
+    output_data = ec.process(text)
+    json.dump(output_data, open(get_data('525_output.jsonl'), 'wt', encoding='utf-8', errors='ignore'))
+
+
+    # print('=============	Enriched Doc 	=============')
+    # print(json.dumps(output_data))
 
 
 if __name__ == '__main__':
